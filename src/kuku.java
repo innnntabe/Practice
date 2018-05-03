2475e1ff6d1965b1a8dfedeaea8d3f817bbb2e32
falgon <falgon53@gmail.com>
2018-05-03 14:18:33 +0900

    タブいれたよん

--- src/kuku.java
+++ src/kuku.java
@@ -4,7 +4,7 @@ public class Kuku2 {
 	public static void main(String[] args) {
         for(int i = 1; i <= 9; i++) {
             for(int j = 1; j <= 9; j++) {
-                System.out.printf("%2d ", i * j);
+                System.out.printf("\t%2d ", i * j);
             }
             System.out.println();
         }
